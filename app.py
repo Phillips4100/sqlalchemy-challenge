092e2261af804adc0023b20abf7a4cca1cd762bf
Nathan Phillips <phillips4100@gmail.com>
2020-08-27 21:32:44 -0600

    updates to flask app

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 
 
@@ -36,12 +36,14 @@ def home():
         f"Available Routes:<br/>"
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/station<br/>"
-        f"/api/v1.0/tobs"
+        f"/api/v1.0/waihee_tobs<br/>"
+        f"/api/v1.0/date<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end>"
     )
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-    
     precipitation = {}
     prcp= engine.execute('SELECT measurement.date, measurement.prcp FROM measurement').fetchall()
     prcp_data= (Convert(prcp, precipitation))
@@ -50,18 +52,52 @@ def precipitation():
 
 @app.route("/api/v1.0/station")
 def measurement():
-    # Create our session (link) from Python to the DB
     session = Session(engine)
-    results = session.query(station.station, station.name).all()
+    result = session.query(station.station, station.name).all()
     session.close()
 
     # Convert list of tuples into dict
     stats = {}
-    stations = (Convert(results, stats))
+    stations = (Convert(result, stats))
 
     return jsonify(stations)
 
+@app.route("/api/v1.0/waihee_tobs")
+def tobs():
+    waihee_tobs={}
+    result1=engine.execute("SELECT date, tobs FROM measurement WHERE date > '2016-08-23' AND station = 'USC00519281'").fetchall()
+    waihee_tobs_data = (Convert(result1, waihee_tobs))
 
+    return jsonify(waihee_tobs_data)
+
+@app.route("/api/v1.0/date")
+def getdate():
+    return """
+        <html>
+            <body>
+                <h3>When is your trip?</h3>
+                <form action = "/date">
+                    Enter your start date (YYYY-MM-DD):<br>
+                    <input type = 'text' name = 'start'><br>
+                    <input type = 'submit' value = 'continue'>
+                </form>
+            </body>
+        </html>
+    """
+
+@app.route("/api/v1.0/date<start>")
+
+def get_temp():
+    try:
+        start = request.form.get('start')
+
+    result2 = engine.execute(f'SELECT MAX(tobs) AS max,  AVG(tobs) AS avg, MIN(tobs) as min FROM measurement Where measurement.date > {Start}').fetchall()
+    start_tobs = list(*result2)
+
+    except:
+    (f'Date not found')
+
+    return jsonify(start_tobs)
 
 if __name__ == '__main__':
     app.run(debug=True)
